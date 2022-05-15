f028c5c28e6526138d175f27773a500764ec7f74
Dmitriy Monakhov <dimamon6032403@gmail.com>
2022-05-15 22:22:06 +0200

    Car mouse following implemented

--- game/Car.js
+++ game/Car.js
@@ -4,11 +4,11 @@ class Car extends Pawn
     {
         super("Car")
         this.location = location       
-        this.rotation = 0
+        this.rotation = 3 * Math.PI / 2
         this.scale = Vector2D.identity()
 
-        this.maximum_speed = 100
-        this.maximum_angular_speed = 0.01
+        this.maximum_speed = 500
+        this.maximum_angular_speed = 0.05
 
         this.drawable = this.create_drawable()   
         this.add_component(this.create_collider())
@@ -42,16 +42,21 @@ class Car extends Pawn
         let input = this.game_instance.input
         let mouse_pos = input.mouse_position
 
+        let face_target = new Vector2D(mouse_pos.x, 0)
+
+        let car_target = Vector2D.subv(face_target, this.location)
+
         let car_mouse = Vector2D.subv(mouse_pos, this.location)
         let direction_car_mouse_length = car_mouse.length
         let direction_car_mouse = Vector2D.divf(car_mouse, direction_car_mouse_length)
         
         let target_rotation = 0
 
+        target_rotation = car_target.rotation_angle
+
         if(direction_car_mouse_length > 0.01)
         {
-            target_rotation = car_mouse.rotation_angle           
-
+                       
             let step = this.maximum_speed * delta_seconds
             if (step > direction_car_mouse_length)
             {
@@ -60,16 +65,18 @@ class Car extends Pawn
 
             let translation = Vector2D.multf(direction_car_mouse, step)
             this.location = Vector2D.addv(this.location, translation)
-        }
-        else
-        {
-            target_rotation = 0
-        }        
-
-        if(Math.abs(this.rotation - target_rotation) > 0.01)
+        }  
+        
+        let rotation_delta = this.rotation - target_rotation
+        let rotation_delta_abs = Math.abs(rotation_delta)
+        if(rotation_delta_abs > 0.01)
         {
             let rotation_direction = get_shortest_rotation(this.rotation, target_rotation)
             let rotation_step = rotation_direction * this.maximum_angular_speed
+            if(Math.abs(rotation_step) > rotation_delta_abs)
+            {
+                rotation_step = rotation_delta
+            }
             this.rotation += rotation_step
         }      
 
